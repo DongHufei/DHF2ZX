e5edf724c07b323b59e51300b3ecd0749663f1c9
DongHufei <mac@dhfdeiMac.local>
2016-03-12 14:57:17 -0800

    donghufei
    add Love

---
+++ Github/Github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 DHF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Github/Github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Github
+//
+//  Created by Mac on 16/3/12.
+//  Copyright © 2016年 DHF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
